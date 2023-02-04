9cd37f579d5bfa1e1aa27620eed4ca7e3a73ae3d
Shashwatjxl <shashwat.samadhiya@jaxl.com>
2023-02-04 16:50:21 +0530

    changes

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -11,108 +13,152 @@ class register_page extends StatefulWidget {
 class _register_pageState extends State<register_page> {
   @override
   Widget Crd(BuildContext context) {
-    return Container(
+    return Flexible(
+        child: Container(
       width: 390,
-      height: 508,
+      //height: 508,
       color: Colors.black,
-      child: Stack(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-              margin: EdgeInsets.only(top: 69, left: 16),
-              width: 358,
-              height: 102,
-              child: RichText(
-                text: TextSpan(
-                    text: 'Activate ',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w300,
-                        fontSize: 28,
-                        height: 1.18),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: 'Call Recording ',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 28,
-                              height: 1.18
-                              //letterSpacing: .36
-                              )),
-                      TextSpan(
-                          text: 'and ',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              fontSize: 28,
-                              height: 1.18
-                              //letterSpacing: .36
-                              )),
-                      TextSpan(
-                          text: 'Cheap International rates ',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 28,
-                              height: 1.18
-                              //letterSpacing: .36
-                              )),
-                      TextSpan(
-                          text: 'on your number',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              fontSize: 28,
-                              height: 1.18
-                              // letterSpacing: .36
-                              )),
-                    ]),
-              )),
-          Container(
-            margin: EdgeInsets.only(left: 16, top: 195),
-            width: 251,
-            height: 21,
-            child: Text(
-              "See how it works? ",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: Color(0xFFF636366),
-                letterSpacing: .36,
+          Expanded(flex: 328, child: Container()),
+          Expanded(
+            flex: 485,
+            child: Container(
+                //margin: EdgeInsets.only(top: 69, left: 16),
+                margin: EdgeInsets.only(left: 16),
+                width: 358,
+                // height: 102,
+                child: RichText(
+                  text: TextSpan(
+                      text: 'Activate ',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: min(
+                              28,
+                              min(MediaQuery.of(context).size.width / 14,
+                                  MediaQuery.of(context).size.height / 30)),
+                          height: 1.18),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: 'Call Recording ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: min(
+                                    28,
+                                    min(
+                                        MediaQuery.of(context).size.width / 14,
+                                        MediaQuery.of(context).size.height /
+                                            30)),
+                                height: 1.18
+                                //letterSpacing: .36
+                                )),
+                        TextSpan(
+                            text: 'and ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontSize: min(
+                                    28,
+                                    min(
+                                        MediaQuery.of(context).size.width / 14,
+                                        MediaQuery.of(context).size.height /
+                                            30)),
+                                height: 1.18
+                                //letterSpacing: .36
+                                )),
+                        TextSpan(
+                            text: 'Cheap International rates ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: min(
+                                    28,
+                                    min(
+                                        MediaQuery.of(context).size.width / 14,
+                                        MediaQuery.of(context).size.height /
+                                            30)),
+                                height: 1.18
+                                //letterSpacing: .36
+                                )),
+                        TextSpan(
+                            text: 'on your number',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontSize: min(
+                                    28,
+                                    min(
+                                        MediaQuery.of(context).size.width / 14,
+                                        MediaQuery.of(context).size.height /
+                                            30)),
+                                height: 1.18
+                                // letterSpacing: .36
+                                )),
+                      ]),
+                )),
+          ),
+          Expanded(flex: 114, child: Container()),
+          Expanded(
+            flex: 100,
+            child: Container(
+              margin: EdgeInsets.only(left: 16),
+              // width: 251,
+              // height: 21,
+              child: Text(
+                "See how it works? ",
+                style: TextStyle(
+                  fontSize: min(
+                      16,
+                      min(MediaQuery.of(context).size.width / 18,
+                          MediaQuery.of(context).size.height / 33)),
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFFF636366),
+                  letterSpacing: .36,
+                ),
               ),
             ),
           ),
-          Container(
-            width: 390,
-            height: 221,
-            margin: EdgeInsets.only(top: 248),
-            child: Image.asset(
-              "assests/images/group.png",
+          // Expanded(flex: 152, child: Container()),
+          Expanded(
+            flex: 1052,
+            child: Container(
+              width: 390,
+              // height: 221,
+              //margin: EdgeInsets.only(top: 248),
+              child: Image.asset(
+                "assests/images/group.png",
+              ),
             ),
-          ),
+          )
         ],
       ),
-    );
+    ));
   }
 
   Widget field(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 72, left: 17),
+      //margin: EdgeInsets.only(top: 72, left: 17),
+      margin: EdgeInsets.only(left: 17),
       width: 358,
-      height: 22,
-      child: Stack(children: const [
-        Text(
-          "Your Mobile Number",
-          style: TextStyle(
-              fontWeight: FontWeight.w400,
-              fontSize: 17,
-              color: Colors.white,
-              letterSpacing: -.15),
-        ),
-      ]),
+      // height: 22,
+      child: Text(
+        "Your Mobile Number",
+        style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: min(
+                17,
+                min(MediaQuery.of(context).size.width / 23,
+                    MediaQuery.of(context).size.height / 47)),
+            color: Colors.white,
+            letterSpacing: -.15),
+      ),
     );
   }
 
   Widget textArea(BuildContext context) {
     return Container(
       width: 358,
-      height: 51,
-      margin: EdgeInsets.only(top: 6, left: 17, right: 15),
+      // height: 51,
+      margin: EdgeInsets.only(left: 17, right: 15),
+      // margin: EdgeInsets.only(top: 6, left: 17, right: 15),
       child: Container(
         child: TextField(
           style: TextStyle(color: Colors.white, letterSpacing: 2),
@@ -126,7 +172,10 @@ class _register_pageState extends State<register_page> {
               hintText: 'eg. +91 673 827 3728',
               hintStyle: TextStyle(
                   color: Color(0xfff8e8e93),
-                  fontSize: 17,
+                  fontSize: min(
+                      17,
+                      min(MediaQuery.of(context).size.width / 23,
+                          MediaQuery.of(context).size.height / 47)),
                   fontWeight: FontWeight.w400,
                   letterSpacing: -.41),
               labelStyle: TextStyle(
@@ -141,8 +190,8 @@ class _register_pageState extends State<register_page> {
                     print("clicked");
                   },
                   child: Container(
-                    width: 47,
-                    height: 47,
+                    width: 37,
+                    // height: 47,
                     decoration: BoxDecoration(
                       color: Color(0xFFF9C63E5),
                       borderRadius: BorderRadius.circular(50),
@@ -158,14 +207,17 @@ class _register_pageState extends State<register_page> {
 
   Widget Footnote(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 23, left: 18, right: 19),
-      width: 353,
-      height: 21,
+      //margin: EdgeInsets.only(top: 23, left: 18, right: 19),
+      // width: 353,
+      // height: 21,
       child: Text(
         "You will recieve a call from us. ",
         textAlign: TextAlign.center,
         style: TextStyle(
-            fontSize: 16,
+            fontSize: min(
+                16,
+                min(MediaQuery.of(context).size.width / 18,
+                    MediaQuery.of(context).size.height / 33)),
             fontWeight: FontWeight.w500,
             color: Color(0xfff636366),
             letterSpacing: .46),
@@ -175,19 +227,67 @@ class _register_pageState extends State<register_page> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      body: SafeArea(
         child: Container(
             color: const Color(0xFFF1C1C1E),
-            constraints: BoxConstraints(maxHeight: 844, maxWidth: 390),
+            //constraints: BoxConstraints(maxHeight: 844, maxWidth: 390),
             child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Crd(context),
-                field(context),
-                textArea(context),
-                Footnote(context)
+                Expanded(
+                    flex: 2419,
+                    child: Column(
+                      children: [
+                        Crd(context),
+                      ],
+                    )),
+                Expanded(
+                  flex: 342,
+                  child: Container(),
+                ),
+                Expanded(
+                  flex: 104,
+                  child: field(context),
+                ),
+                Expanded(flex: 4, child: Container()),
+                Expanded(
+                  flex: 242,
+                  child: textArea(context),
+                ),
+                Expanded(
+                  flex: 109,
+                  child: Container(),
+                ),
+                Expanded(
+                  flex: 150,
+                  child: Footnote(context),
+                ),
+                Expanded(
+                  flex: 476,
+                  child: Container(),
+                ),
               ],
             )),
       ),
     );
   }
 }
+
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SingleChildScrollView(
+//         child: Container(
+//             color: const Color(0xFFF1C1C1E),
+//             constraints: BoxConstraints(maxHeight: 844, maxWidth: 390),
+//             child: Column(
+//               children: [
+//                 Crd(context),
+//                 field(context),
+//                 textArea(context),
+//                 Footnote(context)
+//               ],
+//             )),
+//       ),
+//     );
+//   }
+// }
